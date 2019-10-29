7968475d4cc5acb0007610d2dc80b0a876163361
Dawn Cronin <dawncronin7@gmail.com>
2019-10-29 18:18:55 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,20 @@
 require_relative "blackjack.rb"
 
+def runner
+  welcome
+  total = initial_round
+  while total < 22
+  total = subs
+  total = hit?(total)
+  if total != subs
+  display_card_total(total)
+end
+  if total> 21
+    flag = false 
+  end 
+end
+  end_game(total)
+  
+end
+  
 runner
