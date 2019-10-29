92b663591ed5d3ed969f2057e21b2af2a86b61d0
Dawn Cronin <dawncronin7@gmail.com>
2019-10-29 18:19:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,3 +48,18 @@ end
 def invalid_command
   puts "Please enter a valid command"
 end
+def runner
+  welcome
+  total = initial_round
+  while total < 22
+  total = subs
+  total = hit?(total)
+  if total != subs
+  display_card_total(total)
+end
+  if total> 21
+    flag = false 
+  end 
+end
+  end_game(total)
+end
